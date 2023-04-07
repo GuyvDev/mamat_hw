d9a566775d89ad987b4d748376d1320c0511931b
Guy <guyvt@campus.technion.ac.il>
2023-04-07 19:09:28 +0300

    Submitting hw0

---
+++ HW0/digit_freq.c
@@ -0,0 +1,49 @@
+// 123456789
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define ASCII_CHAR_TO_NUMBER 48
+#define MAX_LEN_STRING 1000
+#define AMOUNT_COUNT_NUMBERS 10
+#define FIRST_COUNTING_NUMBER '0'
+#define LAST_COUNTING_NUMBER '9'
+
+void print_array (int array[], int len);
+
+/**
+ * @breif Count the number of times each number appears in a given string
+ * @return A string that represents the counting,
+ *         the first number represents the amount of 0, 
+ *         the second number represents the amount of 1, and so on
+*/
+int main() {
+
+    char string[MAX_LEN_STRING];
+    int count_numbers[AMOUNT_COUNT_NUMBERS] = { 0 }, i, str_len;
+
+    scanf("%s", string);
+    str_len = strlen(string);
+
+    for (i = 0; i < str_len; i++) {
+        if ((string[i] >= FIRST_COUNTING_NUMBER) &&
+            (string[i] <= LAST_COUNTING_NUMBER)) {
+            count_numbers[string[i] - ASCII_CHAR_TO_NUMBER]++;
+        }
+    }
+
+    print_array(count_numbers, AMOUNT_COUNT_NUMBERS);
+    return 0;
+}
+
+void print_array (int array[], int len)
+{
+    int i;
+
+    for (i = 0; i < len; i++) {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+}
---
+++ HW0/expon.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define max_prime_factor 32
+
+/**
+ * @breif Find the smallest number in his own power that divides a given number
+*/
+
+int main() {
+	int received_num ,power_times , num_prime = 0, holder_of_recieved, i,
+	    smallest_num = 1, bigest_power = 0, primes[max_prime_factor] = { 0 },
+		powers[max_prime_factor] = { 0 };
+	
+	scanf("%d", &received_num);
+	holder_of_recieved = received_num ;
+
+	// Searching all of the primes numbers and their's power in recievd_num
+	for (i = 2; i <= sqrt(received_num); i++) {
+		if (holder_of_recieved % i == 0) {
+			primes[num_prime] = i;
+			power_times = 1;
+
+			/* Build the smallest_num to share the same primes 
+			 * with the received_num with a power of 1 */
+			smallest_num = smallest_num * primes[num_prime];
+
+			while (holder_of_recieved % i == 0) {
+				power_times++;
+				holder_of_recieved /= i;
+			}
+			
+			powers[num_prime] = power_times;
+			num_prime++;
+
+			if (power_times > bigest_power) {
+				bigest_power = power_times;
+			}
+		}
+	}
+
+	// When received_num is prime
+	if (received_num == smallest_num) {
+	printf("%d \n\n", received_num);
+	}
+
+	/* Looking for smallest_num ^ smallest_num, smallest_num share the 
+	 * same primes with received_num, remaines to check the power */
+	if (smallest_num > bigest_power) {
+		printf("%d \n\n", smallest_num);
+	}
+
+	else {
+		while (smallest_num < bigest_power) {
+
+			// Increasing the number with the smallest prime  
+			smallest_num = smallest_num * primes[0];
+		}
+		printf("%d \n\n", smallest_num);
+	}
+
+	return 0;
+}
